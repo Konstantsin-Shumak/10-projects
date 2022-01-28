7165b7d42bb9f079d9d9d2eeca8c4733dbb4751f
Konstantsin Shumak <konstantsin.shumak@gmail.com>
2022-01-28 10:48:20 +0300

    finished 6th project

--- 6 - movie-app/script.js
+++ 6 - movie-app/script.js
@@ -2,35 +2,41 @@ const APIURL = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.d
 const IMGPATH = "https://image.tmdb.org/t/p/w1280";
 const SEARCHAPI = "https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query=";
 
-const main = document.querySelector("main");
+const main = document.getElementById("main");
+const form = document.getElementById("form");
+const search = document.getElementById("search");
 
-const getMovies = async () => {
-    const resp = await fetch(APIURL);
-    const respData = await resp.json();
+const showMovies = (movies) => {
+    //clear main
+    main.innerHTML = "";
 
-    respData.results.forEach(movie => {
-        const { poster_path, title, vote_average } = movie;
+    movies.forEach(movie => {
+        const { poster_path, title, vote_average, overview } = movie;
         const movieEl = document.createElement("div");
         movieEl.classList.add("movie");
 
         movieEl.innerHTML = `
-        <img 
-            src="${IMGPATH + poster_path}"
-            alt="${title}"
-        />
-        <div class="movie-info">
-            <h3>${title}</h3>
-            <span class="${getClassByRate(vote_average)}">${vote_average}</span>
-        </div>
+            <img 
+                src="${IMGPATH + poster_path}"
+                alt="${title}"
+            />
+            <div class="movie-info">
+                <h3>${title}</h3>
+                <span class="${getClassByRate(vote_average)}">
+                    ${vote_average}
+                </span>
+            </div>
+            <div class="overview">
+                <h4>Overview: </h4>
+                ${overview}
+            </div>
         `;
 
         main.appendChild(movieEl);
     });
+};
 
-    return respData;
-}
-
-function getClassByRate(vote) {
+const getClassByRate = (vote) => {
     if (vote >= 8) {
         return "green";
     } else if (vote >= 5) {
@@ -38,6 +44,25 @@ function getClassByRate(vote) {
     } else {
         return "red";
     }
-}
+};
 
-getMovies();
+const getMovies = async (url,) => {
+    const resp = await fetch(url);
+    const respData = await resp.json();
+    showMovies(respData.results)
+};
+
+//initially get fav movies
+getMovies(APIURL);
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const searchTerm = search.value;
+
+    if (searchTerm) {
+        getMovies(SEARCHAPI + searchTerm);
+
+        search.value = "";
+    }
+});
